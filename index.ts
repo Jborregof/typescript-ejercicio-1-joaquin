b931a17ccfec90daf33c1e1706bae8f7b71f684c
Jborregof <joaquin.borrego@a.vedrunasevillasj.es>
2020-11-12 13:15:01 +0100

    final

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 // Tipad
 // Para determinar el tipo de los parámetros de la función, debes dar por hecho que los tipos van a ser siempre los de las dos variables que se añaden en la llamada de la linea 20.
 
+//Definimos sus tipos
 let a: number;
 let b: string;
+//Definimos que puede ser o boolean o number
 let c: boolean | number;
 
 a = 5;
@@ -13,6 +15,7 @@ if (Math.random() >= 0.5) {
   c = 100;
 }
 
+//Definimos que la funcion recibe parametros number y cualquier otro y retorna un numero en caso de que param2 sea numero o string en caso de que param2 no lo sea
 function d(param1: number, param2: any): number | string {
   if (typeof param2 === "number") {
     return param1 + param2;
